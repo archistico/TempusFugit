438cc5d218801114a36a56a7c8aa18bdf6d641bd
Emilie Rollandin <info@archistico.com>
2025-09-27 20:41:34 +0200

    dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -18,42 +18,103 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(private EntityManagerInterface $em) {}
+
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // --- LISTE ---
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // 1.1) If you have enabled the "pretty URLs" feature:
-        // return $this->redirectToRoute('admin_user_index');
-        //
-        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        // Progetti non conclusi (stato != 'Completato'), in scadenza per primi
+        $projects = $this->em->getRepository(Project::class)->createQueryBuilder('p')
+            ->andWhere('p.stato != :done OR p.stato IS NULL')->setParameter('done', 'Completato')
+            ->orderBy('p.dataFineStimata', 'ASC')->addOrderBy('p.dataInizio', 'DESC')
+            ->setMaxResults(8)->getQuery()->getResult();
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirectToRoute('...');
-        // }
+        // Azioni non terminate (status.chiusura = 0), scadenza imminente per prime
+        $actions = $this->em->getRepository(Action::class)->createQueryBuilder('a')
+            ->leftJoin('a.status', 's')->addSelect('s')
+            ->andWhere('s.chiusura = 0 OR s.id IS NULL')
+            ->orderBy('a.deadline', 'ASC')->addOrderBy('a.createdAt', 'DESC')
+            ->setMaxResults(8)->getQuery()->getResult();
 
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        // Ultimi tempi registrati
+        $timeEntries = $this->em->getRepository(TimeEntry::class)->createQueryBuilder('t')
+            ->orderBy('t.startAt', 'DESC')->setMaxResults(8)->getQuery()->getResult();
+
+        // Ultimi movimenti (incassi/spese)
+        $movements = $this->em->getRepository(LedgerMovement::class)->createQueryBuilder('m')
+            ->orderBy('m.data', 'DESC')->setMaxResults(8)->getQuery()->getResult();
+
+        // --- KPI / STATISTICHE SEMPLICI ---
+
+        $now = new \DateTimeImmutable();
+        $dayStart   = $now->setTime(0,0);
+        $weekStart  = $now->modify(('1' === $now->format('N')) ? 'today' : 'last monday')->setTime(0,0);
+        $monthStart = $now->modify('first day of this month')->setTime(0,0);
+
+        $minutes = fn(\DateTimeImmutable $from,\DateTimeImmutable $to) => (int) (
+            $this->em->createQueryBuilder()
+                ->select('COALESCE(SUM(t.durataMin),0)')
+                ->from(TimeEntry::class,'t')
+                ->andWhere('t.startAt >= :f')->andWhere('(t.endAt IS NULL OR t.endAt <= :t)')
+                ->setParameter('f',$from)->setParameter('t',$to)
+                ->getQuery()->getSingleScalarResult()
+        );
+
+        $minsToday  = $minutes($dayStart, $now);
+        $minsWeek   = $minutes($weekStart, $now);
+        $minsMonth  = $minutes($monthStart, $now);
+
+        // Efficienza (fatturabili / totali) nel mese
+        $qTot = $this->em->createQueryBuilder()
+            ->select('COALESCE(SUM(t.durataMin),0)')->from(TimeEntry::class,'t')
+            ->andWhere('t.startAt >= :from')->andWhere('t.startAt <= :to')
+            ->setParameter('from',$monthStart)->setParameter('to',$now);
+        $qBill = clone $qTot; $qBill->andWhere('t.billable = 1');
+        $totMinMonth   = (int)$qTot->getQuery()->getSingleScalarResult();
+        $billMinMonth  = (int)$qBill->getQuery()->getSingleScalarResult();
+        $efficiencyPct = $totMinMonth > 0 ? round($billMinMonth / $totMinMonth * 100) : 0;
+
+        // Entrate vs Spese nel mese
+        $sumMov = function(string $tipo): float {
+            return (float) $this->em->createQueryBuilder()
+                ->select('COALESCE(SUM(m.importo),0)')
+                ->from(LedgerMovement::class,'m')
+                ->andWhere('m.tipo = :tipo')->setParameter('tipo',$tipo)
+                ->andWhere('m.data >= :from')->andWhere('m.data <= :to')
+                ->setParameter('from', (new \DateTimeImmutable('first day of this month'))->setTime(0,0))
+                ->setParameter('to', new \DateTimeImmutable())
+                ->getQuery()->getSingleScalarResult();
+        };
+        $entrateMonth = $sumMov('CREDITO');
+        $speseMonth   = $sumMov('DEBITO');
+
+        return $this->render('admin/dashboard.html.twig', [
+            'projects'       => $projects,
+            'actions'        => $actions,
+            'timeEntries'    => $timeEntries,
+            'movements'      => $movements,
+            'minsToday'      => $minsToday,
+            'minsWeek'       => $minsWeek,
+            'minsMonth'      => $minsMonth,
+            'efficiencyPct'  => $efficiencyPct,
+            'entrateMonth'   => $entrateMonth,
+            'speseMonth'     => $speseMonth,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
             // the name visible to end users
-            ->setTitle('ACME Corp.')
-            // you can include HTML contents too (e.g. to link to an image)
-            ->setTitle('<img src="..."> ACME <span class="text-small">Corp.</span>')
+            ->setTitle('Tempus Fugit')
 
             // by default EasyAdmin displays a black square as its default favicon;
             // use this method to display a custom favicon: the given path is passed
