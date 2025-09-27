92a04d3580785e33cef70fc950df6e025ea14a3e
Emilie Rollandin <info@archistico.com>
2025-09-27 14:23:32 +0200

    entity Client

---
+++ src/Entity/Client.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $denominazione = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $piva = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cf = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $telefono = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $via = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cap = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $citta = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $note = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getDenominazione(): ?string
+    {
+        return $this->denominazione;
+    }
+
+    public function setDenominazione(string $denominazione): static
+    {
+        $this->denominazione = $denominazione;
+
+        return $this;
+    }
+
+    public function getPiva(): ?string
+    {
+        return $this->piva;
+    }
+
+    public function setPiva(?string $piva): static
+    {
+        $this->piva = $piva;
+
+        return $this;
+    }
+
+    public function getCf(): ?string
+    {
+        return $this->cf;
+    }
+
+    public function setCf(?string $cf): static
+    {
+        $this->cf = $cf;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTelefono(): ?string
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono(?string $telefono): static
+    {
+        $this->telefono = $telefono;
+
+        return $this;
+    }
+
+    public function getVia(): ?string
+    {
+        return $this->via;
+    }
+
+    public function setVia(?string $via): static
+    {
+        $this->via = $via;
+
+        return $this;
+    }
+
+    public function getCap(): ?string
+    {
+        return $this->cap;
+    }
+
+    public function setCap(?string $cap): static
+    {
+        $this->cap = $cap;
+
+        return $this;
+    }
+
+    public function getCitta(): ?string
+    {
+        return $this->citta;
+    }
+
+    public function setCitta(?string $citta): static
+    {
+        $this->citta = $citta;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
